7b6ce9335851921e90773dbbb425406746ccf445
dsschapira <dsschapira@gmail.com>
2020-03-18 00:54:09 -0400

    Create layout

--- src/components/ChalkBoard/ChalkBoard.js
+++ src/components/ChalkBoard/ChalkBoard.js
@@ -4,7 +4,36 @@ import './ChalkBoard.css';
 function ChalkBoard() {
     return (
       <div id="chalkBoard">
-        Hello World!
+        <div id="rules-container">
+          <h2>Rules:</h2>
+          <hr />
+          <ol>
+            <li>X plays first.</li>
+            <li>+10 points for a win.</li>
+            <li>+5 points for a draw.</li>
+            <li>+1 point for a block.</li>
+            <li>Lose all points on a loss.</li>
+          </ol>
+          <hr />
+        </div>
+        <div id="play-area">
+          <div id="top-row" className="row">
+            <div className="left-col col" id="slotA"></div>
+            <div className="mid-col col" id="slotB"></div>
+            <div className="right-col col" id="slotC"></div>
+          </div>
+          <div id="mid-row" className="row">
+            <div className="left-col col" id="slotD"></div>
+            <div className="mid-col col" id="slotE"></div>
+            <div className="right-col col" id="slotF"></div>
+          </div>
+          <div id="bot-row" className="row">
+            <div className="left-col col" id="slotG"></div>
+            <div className="mid-col col" id="slotH"></div>
+            <div className="right-col col" id="slotI"></div>
+          </div>
+        </div>
+        <div id="score-area"></div>
       </div>
     );
   }
