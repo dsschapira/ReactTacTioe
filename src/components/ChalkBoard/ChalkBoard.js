7e86d03773a2e192a2a29390ec2306bdbcaade92
dsschapira <dsschapira@gmail.com>
2020-03-19 01:37:13 -0400

    whitespace update

--- src/components/ChalkBoard/ChalkBoard.js
+++ src/components/ChalkBoard/ChalkBoard.js
@@ -9,39 +9,39 @@ function ChalkBoard() {
 
   const {gameState} = useContext(GameContext);
 
-    return (
-      <div id="chalkBoard">
-        <div id="rules-container">
-          <h2>Rules:</h2>
-          <hr />
-          <ol>
-            <li>X plays first.</li>
-            <li>+10 points for a win.</li>
-            <li>+5 points for a draw.</li>
-            <li>+1 point for a block.</li>
-            <li>Lose all points on a loss.</li>
-          </ol>
-          <hr />
+  return (
+    <div id="chalkBoard">
+      <div id="rules-container">
+        <h2>Rules:</h2>
+        <hr />
+        <ol>
+          <li>X plays first.</li>
+          <li>+10 points for a win.</li>
+          <li>+5 points for a draw.</li>
+          <li>+1 point for a block.</li>
+          <li>Lose all points on a loss.</li>
+        </ol>
+        <hr />
+      </div>
+      <GameSpace />
+      <div id="score-area">
+        <div id="player-one-score-container">
+          <h5>Player 1</h5>
+          <span>Score: {gameState.players.one.score}</span>
+          <span>High Score: {gameState.players.one.highScore}</span>
         </div>
-        <GameSpace />
-        <div id="score-area">
-          <div id="player-one-score-container">
-            <h5>Player 1</h5>
-            <span>Score: {gameState.players.one.score}</span>
-            <span>High Score: {gameState.players.one.highScore}</span>
-          </div>
-          <div id="player-two-score-container">
-            <h5>Player 2</h5>
-            <span>Score: {gameState.players.two.score}</span>
-            <span>High Score: {gameState.players.two.highScore}</span>
-          </div>
-        </div>
-        <div id="eraser">
-          <div id="eraser-top"></div>
-          <div id="eraser-bottom"></div>
+        <div id="player-two-score-container">
+          <h5>Player 2</h5>
+          <span>Score: {gameState.players.two.score}</span>
+          <span>High Score: {gameState.players.two.highScore}</span>
         </div>
       </div>
-    );
+      <div id="eraser">
+        <div id="eraser-top"></div>
+        <div id="eraser-bottom"></div>
+      </div>
+    </div>
+  );
   }
   
   export default ChalkBoard;
